6ef73ff4a968c8200414c564bc3511120e980326
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-12-05 10:38:04 -0500

    changed camel case to snake case

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 print("Exercise 1: Fahrenheit to Celcius")
 #Two separate options are shown below for weather or thermometer readings where responses are typically whole numbers or have one decimal place.
 
-weatherFahrenheit = int(input("Please enter the outdoor temperature in Farenheit: "))
-weatherCelsius = int((weatherFahrenheit - 32) * (5/9))
+weather_fahrenheit = int(input("Please enter the outdoor temperature in Farenheit: "))
+weather_celsius = int((weather_fahrenheit - 32) * (5/9))
 
 print()
-print(f"Farenheit: {weatherFahrenheit}")
-print(f"Celcius: {weatherCelsius}")
+print(f"Farenheit: {weather_fahrenheit}")
+print(f"Celcius: {weather_celsius}")
 
 print()
-thermometerFahrenheit = float(input("Please enter your temperature in Farenheit: "))
-thermometerCelsius = format((thermometerFahrenheit - 32) * (5/9),".1f")
+thermometer_fahrenheit = float(input("Please enter your temperature in Farenheit: "))
+thermometer_celsius = format((thermometer_fahrenheit - 32) * (5/9),".1f")
 
 print()
-print(f"Farenheit: {thermometerFahrenheit}")
-print(f"Celcius: {thermometerCelsius}")
+print(f"Farenheit: {thermometer_fahrenheit}")
+print(f"Celcius: {thermometer_celsius}")
